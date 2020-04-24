4508c02179c3c834d927b0a2cdfbe551468128b5
Maks Servatiak <55952903+MaksServatyak@users.noreply.github.com>
2020-04-24 11:15:30 +0300

    TASK1

---
+++ CW_2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+
+void task1() {
+    int* ptrarr = new int[100];
+    int n;
+    cout << "Enter lenght of massive:";
+    cin >> n;
+    cout << "Enter array:";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ptrarr[i];
+    }
+    int average = 0;
+    for (int i = 0; i < n; i++)
+    {
+        average = average + ptrarr[i];
+    }
+    average = average / n;
+    for (int i = 0; i < n; i++)
+    {
+        if (ptrarr[i]>average)
+        {
+            cout << ptrarr[i] << " ";
+        }
+    }
+    delete[] ptrarr;
+}
+int main() {
+    task1();
+    system("pause");
+    return 0;
+}
+//Enter lenght of massive : 3
+//Enter array : 1
+//2
+//1
+//2 Для продолжения нажмите любую клавишу . . .
+
+
+
+//Enter lenght of massive : 5
+//Enter array : 1
+//1
+//1
+//2
+//2
+//2 2 Для продолжения нажмите любую клавишу . . .
+
