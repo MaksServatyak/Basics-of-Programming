732da49c6142d8acc6acb2f286d81f4b3593f9f2
Maks Servatiak <55952903+MaksServatyak@users.noreply.github.com>
2020-04-24 11:33:17 +0300

    Add files via upload
    TASK2

--- CW_2.cpp
+++ CW_2.cpp
@@ -1,53 +1,54 @@
 ﻿#include <iostream>
-#include <cstring>
-
-
 using namespace std;
 
-
-void task1() {
-    int* ptrarr = new int[100];
-    int n;
-    cout << "Enter lenght of massive:";
-    cin >> n;
-    cout << "Enter array:";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> ptrarr[i];
-    }
-    int average = 0;
-    for (int i = 0; i < n; i++)
-    {
-        average = average + ptrarr[i];
-    }
-    average = average / n;
-    for (int i = 0; i < n; i++)
-    {
-        if (ptrarr[i]>average)
-        {
-            cout << ptrarr[i] << " ";
-        }
-    }
-    delete[] ptrarr;
-}
 int main() {
-    task1();
-    system("pause");
+    int n;
+    cout << "Enter size ";
+    cin >> n;
+    int** x = new int* [n];
+    for (int i = 0; i < n; i++)
+        x[i] = new int[n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> x[i][j];
+    
+    cout << endl;
+    for (int j = 0; j < n; j++) {
+        for (int i = n - 1; i >= 0; i--)
+            cout << x[i][j] << " ";
+        cout << endl;
+    }
     return 0;
 }
-//Enter lenght of massive : 3
-//Enter array : 1
-//2
-//1
-//2 Для продолжения нажмите любую клавишу . . .
+
+
+//Enter size 3
+//1 2 3
+//4 5 6
+//7 8 9
+//
+//7 4 1
+//8 5 2
+//9 6 3
 
 
 
-//Enter lenght of massive : 5
-//Enter array : 1
-//1
-//1
-//2
-//2
-//2 2 Для продолжения нажмите любую клавишу . . .
+//Enter size 2
+//1 2
+//4 5
+//
+//4 1
+//5 2
+//
 
+
+//Enter size 4
+//1 2 3 5
+//4 5 6 8
+//5 5 8 5
+//5 5 5 2
+//
+//5 5 4 1
+//5 5 5 2
+//5 8 6 3
+//2 5 8 5
