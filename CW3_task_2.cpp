eb9f68383d316db8e56f72309cde1edc8b40dc82
Maks Servatiak <55952903+MaksServatyak@users.noreply.github.com>
2020-05-22 12:46:29 +0300

    Add files via upload

---
+++ CW3_task_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <fstream>
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	
+	ifstream fout("in1.txt");
+	ifstream file("in2.txt");
+	ofstream fin("out.txt", ios_base::app);
+	char arr[100];
+	if (fin.is_open())
+	{
+		fout.getline(arr, 100);
+		fin << arr;
+		file.getline(arr, 100);
+		fin << arr;
+		fin.close();
+		fout.close();
+		file.close();
+
+	}
+	else
+	{
+		cout << "Error" << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
