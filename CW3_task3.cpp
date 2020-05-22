cecaedaa341dd8dab0ad5d9afeaa5604ef5182d7
Maks Servatiak <55952903+MaksServatyak@users.noreply.github.com>
2020-05-22 12:59:13 +0300

    Add files via upload

---
+++ CW3_task3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include<cstring>
+using namespace std;
+struct Kolo
+{
+    //поля
+    int radius;
+    int length;
+    //методи
+    float Area()
+    {
+        float s = 3.14 * radius * radius;
+        return s;
+    }
+    //конструктори
+    Person(int radius,int length)
+    {
+        (*this).length = length;
+
+       
+        (*this).radius = radius;
+    }
+};
+
+int main()
+{
+    Kolo kolo{ 3,45 };
+    cout<<kolo.Area;
+    return 0;
+}
+
+
